18f2612298f20f97b62aa937c80933a9701c00ab
ryudev <usuario@MacBook-Pro-de-usuario.local>
2024-09-24 00:42:01 -0500

    Implementation of dotnev, request for weather API and return of simple text

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const express = require("express");
-const request = require("request");
+const express = require('express');
+const requestPackage = require('request');
+const dotenv = require('dotenv').config();
 
+const SECRET_KEY = process.env.API_KEY;
 const app = express();
 
 app.get("/", (request, response) =>{
-    response.send("Website under construction ...");
+    let city = request.query.city;
+    requestPackage(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${SECRET_KEY}`, (errorApi, responseApi, bodyApi) => {
+        if(responseApi.statusCode === 200){
+            let data = JSON.parse(bodyApi);
+            console.log(data.weather[0]);
+            response.send(`The weather in your city ${city} is ${data.weather[0].description}`)
+        }
+    });
 });
 
 app.listen(3000, () => {
